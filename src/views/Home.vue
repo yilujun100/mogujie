bf2e543af894e0be57316b6967489c3b01b503c4
yilujun100 <yilujun100@qq.com>
2021-09-14 22:21:05 +0800

    feat: add css架构之Tools层

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <header>
+        CSS 架构
+    </header>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-};
+    name: 'Home',
+    components: {}
+}
 </script>
+
+<style lang="scss" scoped>
+header {
+    width: 200px;
+    height: 200px;
+    background: rgba(0, 0, 0, 0.4);
+    color: #fff;
+    @include box-center(center, center);
+}
+</style>
